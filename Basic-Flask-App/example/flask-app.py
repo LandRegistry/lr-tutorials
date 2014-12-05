0bc555c8c83f623399fb993f9e3d5e857fe4f9c4
Skablam <stainlesssteelmatt@gmail.com>
2014-12-05 23:49:45 +0000

    fleshed out tutorial README

--- Basic-Flask-App/example/flask-app.py
+++ Basic-Flask-App/example/flask-app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/hello')
+def hello():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
